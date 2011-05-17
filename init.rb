6866ae4e77ba1063c3b6ed3b36ec55024fdcb823
Laas Toom <laas.toom@gmail.com>
2011-05-17 19:34:23 +0300

    Moved init.rb to where Rails looks for in a gem

--- init.rb
+++ init.rb
@@ -1,3 +1,1 @@
-require File.join(File.dirname(__FILE__), 'lib', 'hide_attributes.rb')
-
-ActiveRecord::Base.send :include, HideAttributes
+require File.join(File.dirname(__FILE__), 'rails',  'init')
---
+++ rails/init.rb
@@ -0,0 +1,3 @@
+require File.join(File.dirname(__FILE__), '..', 'lib', 'hide_attributes.rb')
+
+ActiveRecord::Base.send :include, HideAttributes
